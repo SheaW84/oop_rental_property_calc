c29e394ae90b1b41755a71c37471565016888393
SheaW84 <sheawilsn@gmail.com>
2023-12-13 20:25:29 -0600

    lots of refactoring

--- income.py
+++ income.py
@@ -11,7 +11,8 @@ class ROICalculator():
             totalMonthlyIncome() expects the amount for rent, laundry, storage, and miscellaneous items
             and returns the total estimated amount for the month.
         """
-        rent_amount = int(input("How much is the rent?: "))
+        print("\nFirst, let's figure out your monthly income.")
+        rent_amount = int(input("\nHow much is the rent?: "))
         laundry_amount = int(input("How much is the laundry?: "))
         storage_amount = int(input("How much is storage?: "))
         misc_amount =  int(input("Total miscellaneous charges for your tenant?: "))
@@ -19,7 +20,8 @@ class ROICalculator():
         self.total_income = rent_amount + laundry_amount + \
             storage_amount + misc_amount
         
-        return self.total_income
+        print(f"Monthy Income: {self.total_income}")
+        
     
     def monthlyExpences(self):
         """
@@ -27,7 +29,8 @@ class ROICalculator():
             and returns the total of that for the month
 
         """
-        taxes = int(input("What are the taxes on the property?: "))
+        print("\nSecond, let's go through your monthly expenses.")
+        taxes = int(input("\nWhat are the taxes on the property?: "))
         insurance = int(input("What are the monthly insurance payments?: "))
         water = int(input("Water cost?: "))
         waste = int(input("Waste cost?: "))
@@ -47,7 +50,7 @@ class ROICalculator():
             vacancy + repairs + capEx + \
             propertyManagement + mortgage
         
-        return self.total_expense
+        print(f'Monthly Expenses: {self.total_expense}') 
     
     def cashFlowMonthly(self):
         """
@@ -56,7 +59,7 @@ class ROICalculator():
         monthly_cash_flow = self.total_income - self.total_expense
         self.annual_cash_flow = monthly_cash_flow * 12
 
-        return self.annual_cash_flow
+        print(f'\nAnnual Cash flow: {self.annual_cash_flow}')
     
     def cashOnCash(self):
         """
@@ -64,7 +67,9 @@ class ROICalculator():
             and sums it to the total_investment. 
 
         """
-        downpayment = int(input("Planned downpayment?: "))
+
+        print("\nNow lets figure out your Cash on Cash return")
+        downpayment = int(input("\nPlanned downpayment?: "))
         closing_cost = int(input("Discussed closing cost?: "))
         rehab_budget = int(input("Rehab budget?: "))
         misc_expenses = int(input("Other expenses?:"))
@@ -73,20 +78,34 @@ class ROICalculator():
             rehab_budget + misc_expenses
         
         self.cash_return = self.annual_cash_flow / total_investment * 100
+        print(f'Total investment: {total_investment}')
         
-        print(f"Cash on cash return %{float(self.cash_return,2)}")
 
     def run(self):
 
+        on = True
+        while on:
 
-        print("This is the Return On Investment Calculator")
-        new_ROI.totalMonthlyIncome()
-        new_ROI.monthlyExpences()
-        new_ROI.cashFlowMonthly()
-        new_ROI.cashOnCash()
+            print("This is the Return On Investment Calculator")
+            new_ROI.totalMonthlyIncome()
+            new_ROI.monthlyExpences()
+            new_ROI.cashFlowMonthly()
+            new_ROI.cashOnCash()
+            print("\n Summary of figures")
+            print(f'\nTotal monthly income: {self.total_income}')
+            print(f'Total monthly expenses: {self.total_expense}')
+            print(f'Total annual cashflow: {self.annual_cash_flow} ')
+            print(f"\nCash on cash return {round(float(self.cash_return),2)}")
 
-
-
+            guest = input("\nDo you wish to evaluate another property? Please type 'y' or 'n' : ")
+            if guest.lower() == 'n':
+                print("Thank you for using the calculator have a great day!")
+                break
+            elif guest.lower() == 'y':
+                continue
+            else:
+                print('Please type a choice from the list provided above!')
+                break
 
 
 new_ROI = ROICalculator(1,1,1,1)
